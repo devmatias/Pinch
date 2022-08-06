7fec7b8c56ebdb33c6f886a1339d071b5796b0aa
devmatias <devmatias@gmail.com>
2022-08-06 11:35:58 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Matias Correa Franco de Faria on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
